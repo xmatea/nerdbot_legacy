48520829ed79cddf0a7be8b52e46ec3e9bdb7250
unknown <mateabjork@gmail.com>
2021-03-28 23:19:58 +0200

    add img_to_colour command

--- cogs/coding.py
+++ cogs/coding.py
@@ -7,6 +7,7 @@ import requests
 from io import BytesIO
 import numpy
 import html_module
+from types import SimpleNamespace
 
 
 config = readjson('config.json')
@@ -24,16 +25,17 @@ class Coding(commands.Cog):
         if not ctx.message.attachments:
             raise commands.BadArgument()
 
-        #url = ctx.message.attachments[0].url
-        url = "https://cdn.discordapp.com/attachments/825789854738677811/825803542388408330/apricat.jpg"
+        url = ctx.message.attachments[0].url
         img = Image.open(requests.get(url, stream=True).raw)
         na = numpy.array(img.convert('RGB'))
-        rgb_r = na.mean(axis=0).mean(axis=0)
-        print(rgb_r)
-        print(img.load()[1,1])
-        hex_r = '%02x%02x%02x' % rgb_r.tolist()
-        embed = discord.Embed(title="colour", colour=colour_convert(hex_r))
-        embed.description = f"{hex_r}"
+
+        rgb = [int(i) for i in na.mean(axis=0).mean(axis=0)]
+        hex = '%02x%02x%02x' % tuple(rgb)
+
+        embed = discord.Embed(title="Mean colour", colour=colour_convert(hex), description="Image mean colour value:")
+        embed.add_field(name="HEX", value=hex)
+        embed.add_field(name="RGB", value=f"({tuple(rgb)[0]}, {tuple(rgb)[1]}, {tuple(rgb)[2]})")
+        embed.add_field(name="Integer", value=colour_convert(hex))
         await ctx.send(content="", embed=embed)
 
 
@@ -41,14 +43,14 @@ class Coding(commands.Cog):
     async def html_to_img(self, ctx, *, html=None):
         if html is None:
             if not ctx.message.attachments:
-                raise commands.BadArgument
+                raise commands.MissingRequiredArgument(SimpleNamespace(name="html"))
 
             url = ctx.message.attachments[0].url
             html = requests.get(url).text
 
         img = await html_module.convert_to_img(html)
-        img.seek(0)
 
+        img.seek(0)
         await ctx.send(file=discord.File(img, "image.png"))
 
 
