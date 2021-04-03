52769b8246fd33ebef3f73999530d80a97f98e0a
xmatea <mateabjork@gmail.com>
2021-04-03 18:02:50 +0200

    split coding.py and images.py and update helptext

--- cogs/coding.py
+++ cogs/coding.py
@@ -1,14 +1,8 @@
 import discord
 from discord.ext import commands
 from process import readjson, colour_convert
-import random
-from PIL import Image
 import requests
-from io import BytesIO
-import numpy
 import html_module
-import image_processing
-import random
 
 
 config = readjson('config.json')
@@ -20,56 +14,6 @@ class Coding(commands.Cog):
         self.bot = bot
         self.hidden = False
         self.name = 'Coding'
-
-    @commands.command(help=speech.help.palette, brief=speech.brief.palette)
-    async def palette(self, ctx):
-        url = ctx.message.attachments[0].url
-        img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
-
-        palette = image_processing.generate_palette(img)
-        palette.seek(0)
-
-        await ctx.send(file=discord.File(palette, "palette.png"))
-
-    @commands.group(help=speech.help.colour_from_img, brief=speech.brief.colour_from_img, aliases=["color_from_img"])
-    async def colour_from_img(self, ctx):
-        if ctx.invoked_subcommand is None:
-            raise commands.UserInputError()
-
-    @colour_from_img.command(aliases=["-m"])
-    async def mean(self, ctx):
-        if not ctx.message.attachments:
-            raise commands.UserInputError()
-
-        if ctx.invoked_subcommand is None:
-            url = ctx.message.attachments[0].url
-            img = Image.open(requests.get(url, stream=True).raw)
-            na = numpy.array(img.convert('RGB'))
-
-            rgb = tuple([int(i) for i in na.mean(axis=0).mean(axis=0)])
-            hx = '%02x%02x%02x' % rgb
-
-            embed = discord.Embed(title="Mean colour", colour=colour_convert(hx), description="Image mean colour value:")
-            embed.add_field(name="HEX", value=hx)
-            embed.add_field(name="RGB", value=f"({rgb[0]}, {rgb[1]}, {rgb[2]})")
-            embed.add_field(name="Integer", value=colour_convert(hx))
-            await ctx.send(content="", embed=embed)
-
-
-    @colour_from_img.command(aliases=["-r"])
-    async def random(self, ctx):
-        url = ctx.message.attachments[0].url
-        img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
-
-        rgb = random.choice(list(zip(*(iter(numpy.array(img.convert('RGB')).flatten().tolist()),) * 3)))
-        hx = '%02x%02x%02x' % rgb
-
-        embed = discord.Embed(title="Random colour", colour=colour_convert(hx), description="Image mean colour value:")
-        embed.add_field(name="HEX", value=hx)
-        embed.add_field(name="RGB", value=f"({rgb[0]}, {rgb[1]}, {rgb[2]})")
-        embed.add_field(name="Integer", value=colour_convert(hx))
-        await ctx.send(content="", embed=embed)
-
 
     @commands.command(help=speech.help.html_to_img, brief=speech.brief.html_to_img)
     async def html_to_img(self, ctx, *, html=None):
--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from process import readjson
+from datetime import datetime
 
 Config = readjson('config.json')
 speech = readjson('speech.json')
@@ -15,8 +16,8 @@ class Help(commands.Cog):
     async def help(self, ctx, *args):
         if not args:
             # List all commands in an embed
-            embed = discord.Embed(title=":hibiscus: ♡ Help ♡ :hibiscus:",colour=15643828)
-            embed.description = f"aaaaa"
+            embed = discord.Embed(title=":bar_chart: Help :bar_chart:", colour=int(speech.helpembed.embedcolour), timestamp=datetime.now())
+            embed.description = f"{speech.helpembed.helptext}".format(Config.prefix)
 
             for c in self.bot.cogs:
                 cog = self.bot.get_cog(c)
@@ -28,15 +29,11 @@ class Help(commands.Cog):
                     embed.add_field(name=c, value=text)
             await ctx.send(content="", embed=embed)
 
-        elif len(args) > 1:
-            await ctx.send("You're making it hard for me... Please specify only one command or category, and I'll help you with that.")
-
-
         elif self.bot.get_cog(args[0].lower().capitalize()):
             cog = self.bot.get_cog(args[0].lower().capitalize())
             if not cog.hidden:
                 text=""
-                embed = discord.Embed(title=f":hibiscus: ♡ {cog.name} commands ♡ :hibiscus:",colour=15643828)
+                embed = discord.Embed(title=f"{cog.name} commands",colour=int(speech.helpembed.embedcolour))
                 for cmd in cog.get_commands():
                     if not cmd.hidden:
                         text += f"{cmd.name} - {cmd.brief}\n"
@@ -47,7 +44,7 @@ class Help(commands.Cog):
         elif self.bot.get_command(args[0].lower()):
             cmd = self.bot.get_command(args[0].lower())
             if not cmd.hidden:
-                embed = discord.Embed(title=f":hibiscus: ♡ {cmd.name} ♡ :hibiscus:",colour=15643828)
+                embed = discord.Embed(title=f"{cmd.name}",colour=int(speech.helpembed.embedcolour))
                 text = f"{cmd.help}".format(Config.prefix)
                 embed.add_field(name="Help and usage", value=text)
                 await ctx.send(content="", embed=embed)
